f6da3acf63ba4c16ba981ba97d0855f1fd818f82
sbzapata <sbzapata@sigmaingenieria.net>
2024-10-04 19:33:08 -0500

    Vue JS

---
+++ index.vue
@@ -0,0 +1,26 @@
+<template>
+    <div>
+        <p>hola</p>
+        <div>
+            {{ text }}
+        </div>
+    </div>
+</template>
+
+
+<script>
+    export default {
+        data() {
+            return {
+                text: "hola vue"
+            };
+        }
+    };
+</script>
+
+<style>
+    p 
+    {
+        color: red;
+    }
+</style>
